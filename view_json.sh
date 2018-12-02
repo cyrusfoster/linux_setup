4aafb95a084d5187276a70a6909ba01a3e0a2142
Pluto <yo@yo.com>
2018-12-02 08:33:33 -0800

    html indent

--- view_json.sh
+++ view_json.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cat $1 |
-  sed -E '/^(var|;)/ d' |  # remove lines starting keywords (not JSON)
   python -m json.tool |  # pretty print
   vim -R -   # open in vim read-only from stdin
 
